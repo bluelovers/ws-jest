32ebde36acb7d30f49e0004cdc65ee04d4c403d7
bluelovers <codelovers@users.sourceforge.net>
2020-06-27 01:41:30 +0800

    build(matchers): update index of matchers

--- packages/jest-extended-extra/lib/matchers.d.ts
+++ packages/jest-extended-extra/lib/matchers.d.ts
@@ -1,18 +1,15 @@
 /// <reference types="jest" />
 export declare const matchers: {
     toBeFloat: (this: jest.MatcherContext, received: number) => {
-        pass: boolean;
+        pass: any;
         message: () => string;
     };
 } & {
     toBeInteger: (this: jest.MatcherContext, received: number) => {
-        pass: boolean;
+        pass: any;
         message: () => string;
     };
 } & {
-    toBeZero: (this: jest.MatcherContext, received: number) => {
-        pass: boolean;
-        message: () => string;
-    };
+    toBeZero: typeof import("./matchers/toBeZero").toBeZero;
 };
 export default matchers;

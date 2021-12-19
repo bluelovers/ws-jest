1e98ea3df7b733e2d4be6957e42188c747eb80bf
bluelovers <codelovers@users.sourceforge.net>
2021-12-20 02:10:06 +0800

    .

--- packages/chai-asserttype-extra/test/temp.ts
+++ packages/chai-asserttype-extra/test/temp.ts
@@ -2,11 +2,11 @@
  * Created by user on 2018/11/15/015.
  */
 
-import ChaiPluginAssertType from '../src';
+import { install } from '../src';
 import _chai from 'chai';
 
 //chai.use(ChaiPluginAssertType);
-const chai = ChaiPluginAssertType.install(_chai);
+const chai = install(_chai);
 
 chai.expect(1).is.number()
 _chai.expect(1.1).integer.finite

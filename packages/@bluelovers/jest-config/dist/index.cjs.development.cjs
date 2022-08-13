33217361c143aa7d157b26e6077a6b020ecd65db
bluelovers <codelovers@users.sourceforge.net>
2022-08-13 08:45:54 +0800

    build(dist): update build

--- packages/@bluelovers/jest-config/dist/index.cjs.development.cjs
+++ packages/@bluelovers/jest-config/dist/index.cjs.development.cjs
@@ -78,7 +78,7 @@ function defaultTestPathIgnorePatterns() {
 }
 
 var name = "@bluelovers/jest-config";
-var version = "1.0.6";
+var version = "1.0.7";
 
 function _newTableBorderless(options) {
   let table$1 = new table.Table({

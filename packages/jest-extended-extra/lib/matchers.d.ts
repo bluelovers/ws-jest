84856a60569c11269e282ca19a92a023765ce4ec
bluelovers <codelovers@users.sourceforge.net>
2022-09-15 07:27:22 +0800

    build(matchers): update index of matchers

--- packages/jest-extended-extra/lib/matchers.d.ts
+++ packages/jest-extended-extra/lib/matchers.d.ts
@@ -3,15 +3,9 @@ export declare const matchers: {
     readonly toBeZero: typeof import("./matchers/toBeZero").toBeZero;
     readonly toBePositive: typeof import("./matchers/toBePositive").toBePositive;
     readonly toBeNegative: typeof import("./matchers/toBeNegative").toBeNegative;
-    readonly toBeInteger: (this: jest.MatcherContext, received: number) => {
-        pass: boolean;
-        message: () => string;
-    };
+    readonly toBeInteger: (this: jest.MatcherContext, received: number) => jest.CustomMatcherResult;
     readonly toBeInfinity: typeof import("./matchers/toBeInfinity").toBeInfinity;
-    readonly toBeFloat: (this: jest.MatcherContext, received: number) => {
-        pass: boolean;
-        message: () => string;
-    };
+    readonly toBeFloat: (this: jest.MatcherContext, received: number) => jest.CustomMatcherResult;
     readonly toBeFinite: typeof import("./matchers/toBeFinite").toBeFinite;
 };
 export default matchers;

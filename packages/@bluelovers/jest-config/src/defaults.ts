cec12f65cc45ff10b48f2582d9e63a9ad5c4a08f
bluelovers <codelovers@users.sourceforge.net>
2022-10-30 05:06:49 +0800

    fix: avoid jest load .ts file first

--- packages/@bluelovers/jest-config/src/defaults.ts
+++ packages/@bluelovers/jest-config/src/defaults.ts
@@ -6,6 +6,8 @@ import { InitialOptionsTsJest } from 'ts-jest';
 import { defaultTsJestTransformerOptions } from './plugin/ts-jest';
 import { IRuntime } from './types';
 
+import { defaults } from 'jest-config';
+
 export function defaultTestFileExtensions()
 {
 	const value = [
@@ -24,16 +26,9 @@ export function defaultTestFileExtensions()
 export function defaultModuleFileExtensions()
 {
 	const value = [
-		'ts',
-		'tsx',
+		...defaults.moduleFileExtensions as ["js", "mjs", "cjs", "jsx", "ts", "tsx", "json", "node"],
 		'mts',
 		'cts',
-		'js',
-		'jsx',
-		'mjs',
-		'cjs',
-		'json',
-		'node',
 	] as const
 	return value as ITSToWriteableArray<typeof value>;
 }
